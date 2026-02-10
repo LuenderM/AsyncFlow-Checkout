ab30f508b3b8f0b1b3a04c9bf5cc1eaeacabd365
Luender Meira <luender.meira@gmail.com>
2026-02-10 00:07:54 -0300

    chore: Update PostgreSQL image to version 17, refactor RabbitMQ configuration, and remove unused AsyncFlowCheckoutApplication class

--- src/main/java/com/asyncflow/checkout/api/AsyncFlowCheckoutApplication.java
+++ src/main/java/com/asyncflow/checkout/api/AsyncFlowCheckoutApplication.java
@@ -1,4 +1,4 @@
-package com.asyncflow.checkout_api;
+package com.asyncflow.checkout.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/asyncflow/checkout/api/config/RabbitMQConfig.java
+++ src/main/java/com/asyncflow/checkout/api/config/RabbitMQConfig.java
@@ -1,6 +1,8 @@
 package com.asyncflow.checkout.api.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,8 +12,12 @@ public class RabbitMQConfig {
     public static final String QUEUE_NAME = "orders-queue";
 
     @Bean
-    public Queue ordersQueue() {
+    public Queue queue() {
         return new Queue(QUEUE_NAME, true);
     }
 
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
 }
